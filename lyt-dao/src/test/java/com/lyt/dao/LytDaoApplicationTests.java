646d510924a17086bfb576b6a7552fed6abec75d
liyintao <lyt890916wlj@sina.com>
2020-04-23 19:00:13 +0800

    springboot2.2.6配置数据源发生变更了

--- lyt-dao/src/test/java/com/lyt/dao/LytDaoApplicationTests.java
+++ lyt-dao/src/test/java/com/lyt/dao/LytDaoApplicationTests.java
@@ -1,8 +1,11 @@
 package com.lyt.dao;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootTest
 class LytDaoApplicationTests {
 
--- my-shop/src/main/java/com/lyt/shop/MyShopApplication.java
+++ my-shop/src/main/java/com/lyt/shop/MyShopApplication.java
@@ -1,8 +1,11 @@
 package com.lyt.shop;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @SpringBootApplication
 public class MyShopApplication {
 
