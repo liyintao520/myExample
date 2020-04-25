36a80aa13f3a29b5cb76133bc807400f4f07e6c4
liyintao520 <lyt890916wlj@sina.com>
2020-04-25 23:59:18 +0800

    问题： 因为打包打不到my-shop中。 进行了二次打包。 解决：dao的xml修改。去掉打包插件

--- my-shop/src/main/java/com/lyt/controller/HelloController.java
+++ my-shop/src/main/java/com/lyt/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.lyt.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,7 @@ import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/")
-//@Slf4j
+@Slf4j
 public class HelloController {
 
     @Autowired
