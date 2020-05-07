474dc5afafc31f5b426e3bb5b6ae6f2ca812c0d4
liyintao <lyt890916wlj@sina.com>
2020-05-07 11:00:39 +0800

    集合test

---
+++ lyt-example/src/main/java/com/lyt/listTest/ListTest.java
@@ -0,0 +1,11 @@
+package com.lyt.listTest;
+
+/**
+ * @ClassName ListTest
+ * @Description TODO
+ * @Author liyintao
+ * @Date 2020/5/7 11:00
+ */
+public class ListTest {
+
+}
---
+++ lyt-example/src/main/java/com/lyt/setTest/BianLiSet.java
@@ -0,0 +1,50 @@
+package com.lyt.setTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @ClassName BianLiSet
+ * @Description Java HashSet遍历的2种方法
+ * @Author liyintao
+ * @Date 2020/5/7 10:54
+ */
+@Slf4j
+public class BianLiSet {
+
+    public static void main(String[] args) {
+        one(getSet());
+        two(getSet());
+    }
+    public static Set<String> getSet () {
+        Set<String> set = new HashSet<String>();
+        set.add("aaa");
+        set.add("bbb");
+        set.add("ccc");
+        return set;
+    }
+
+    /**
+     * 1. Iterator迭代方式遍历
+     * @param set
+     */
+    public static void one (Set<String> set) {
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    /**
+     * 2. for循环方式遍历
+     * @param set
+     */
+    public static void two (Set<String> set) {
+        for (String s:set) {
+            System.out.println(s);
+        }
+    }
+}
