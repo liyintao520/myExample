c27a213a5dda55f098f03dd48d2d8e1000fee740
liyintao520 <lyt890916wlj@sina.com>
2020-05-10 23:53:40 +0800

    线程demo

---
+++ lyt-example/src/main/java/threadcoreknowledge/createthreads/ThreadStyle.java
@@ -0,0 +1,24 @@
+package threadcoreknowledge.createthreads;
+
+/**
+ * 描述：     用Thread方式实现线程
+ * 重写Thread的run()方法（集成Thread类）
+ * java中类是单继承的  java接口是可以多继承的
+ * 整个run()方法被重写了。可以看一下Thread的run()方法。746行
+ */
+public class ThreadStyle extends Thread{
+
+    @Override
+    public void run() {
+        System.out.println("用Thread类实现线程");
+    }
+
+    public static void main(String[] args) {
+        new ThreadStyle().start();
+    }
+}
+
+
+
+
+
