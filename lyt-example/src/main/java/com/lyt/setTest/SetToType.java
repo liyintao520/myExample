e0137fb7b9ec20fa796b44f9355bfd824afb6e3b
liyintao <lyt890916wlj@sina.com>
2020-05-11 18:57:24 +0800

    优化代码

--- lyt-example/src/main/java/com/lyt/setTest/SetToType.java
+++ lyt-example/src/main/java/com/lyt/setTest/SetToType.java
@@ -7,7 +7,7 @@ import java.util.Set;
 
 /**
  * @ClassName SetToType
- * @Description TODO
+ * @Description Set<T> 集合转换成其他类型 </>
  * @Author liyintao
  * @Date 2020/5/11 13:37
  */
@@ -16,8 +16,13 @@ public class SetToType {
 
     public static void main(String[] args) {
         log.info(JsonUtil.toJson(setToArray()));
+
     }
 
+    /**
+     * Set<String> 转换 String[] 数组</>
+     * @return
+     */
     public static String[] setToArray() {
         Set<String> setKeys = InitSetDb.getSet();
         String[] keysStr = setKeys.toArray(new String[setKeys.size()]);
