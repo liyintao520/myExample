68be2183976ab927dca512bfc49fdabd70389c7a
liyintao520 <lyt890916wlj@sina.com>
2020-05-25 23:28:14 +0800

    0525，run和start方法

---
+++ lyt-example/src/main/java/threadcoreknowledge/startthread/StartAndRunMethod.java
@@ -0,0 +1,20 @@
+package threadcoreknowledge.startthread;
+
+import threadcoreknowledge.createthreads.ThreadStyle;
+
+/**
+ * 描述：     对比start和run两种启动线程的方式
+ */
+public class StartAndRunMethod {
+
+    public static void main(String[] args) {
+        Runnable runnable = () -> {
+            System.out.println(Thread.currentThread().getName());
+
+        };
+        // 主线程执行的
+        runnable.run();
+        // 是调用子线程来实现启动线程的
+        new Thread(runnable).start();
+    }
+}
