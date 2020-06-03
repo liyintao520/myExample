bebb30ef972fd61f575512d28c3d8630a67527fd
liyintao <lyt890916wlj@sina.com>
2020-06-03 21:08:22 +0800

    OCR图片识别文字 demo（需要安装Tesseract-OCR）配置环境变量

---
+++ lyt-example/src/main/java/com/lyt/orc/TestOcrUtils.java
@@ -0,0 +1,48 @@
+package com.lyt.orc;
+
+/**
+ * @ClassName TestOcrUtils
+ * @Description TODO
+ * @Author liyintao
+ * @Date 2020/6/3 16:42
+ */
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+public class TestOcrUtils {
+
+    public static void main(String[] args) {
+//        System.out.println(getImgText("C:/Users/Administrator/Desktop/图片/1.gif"));
+//        System.out.println(getImgText("C:/Users/Administrator/Desktop/图片/3.jpg"));
+        System.out.println(getImgText("C:/Users/Administrator/Desktop/图片/3.png"));
+//        System.out.println(getImgText("C:/Users/Administrator/Desktop/图片/5.png"));
+//        System.out.println(getImgText("C:/Users/Administrator/Desktop/图片/25.jpg"));
+    }
+
+    public static String  getImgText(String imageLocation) {
+        ITesseract instance = new Tesseract();
+        instance.setDatapath("D:/IDEA/Tesseract-OCR/tessdata");
+        try {
+            instance.setLanguage("chi_sim");
+            File imageFile = new File(imageLocation);
+//			 String imgText = instance.doOCR();
+            BufferedImage bi  = ImageIO.read(imageFile);
+            return instance.doOCR(bi);
+        } catch (TesseractException e) {
+            e.getMessage();
+            return "Error while reading image";
+        }catch (IOException e) {
+            e.getMessage();
+            return "Error while reading image";
+        }
+    }
+
+}
